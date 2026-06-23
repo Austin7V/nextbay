9d8f4f5cfbeabf85db4e298b6ce28f94b03ef35f
Sergey Badin <sergeykfg1@me.com>
2026-06-23 10:50:37 +0200

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+import { loginAction } from "@/lib/authActions";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <form action={loginAction}>
+        <label htmlFor="username">Username</label>
+        <input id="username" name="username" type="text" required />
+
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" required />
+
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  );
+}
